f57de1310b5404338a32403784c4d5e8a03d0ada
Holst <moviemastersdk@gmail.com>
2017-05-01 09:06:43 +0200

    no message

--- AsteroidsNetbeansModules/OSGiEnemy/src/main/java/dk/sdu/mmmi/cbse/osgienemy/EnemySystem.java
+++ AsteroidsNetbeansModules/OSGiEnemy/src/main/java/dk/sdu/mmmi/cbse/osgienemy/EnemySystem.java
@@ -1,4 +1,3 @@
-
 package dk.sdu.mmmi.cbse.osgienemy;
 
 import dk.sdu.mmmi.cbse.common.data.EntityType;
@@ -9,25 +8,20 @@ import dk.sdu.mmmi.cbse.common.events.EventType;
 import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
 import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
 
-
-public class EnemySystem implements IEntityProcessingService, IGamePluginService{
-
+public class EnemySystem implements IEntityProcessingService, IGamePluginService {
 
     @Override
     public void start(GameData gameData, World world) {
-        System.out.println("I STARTED YAY");
+        gameData.addEvent(new Event(EventType.PLAYER_SHOOT, world.getEntities(EntityType.PLAYER).get(0).getID()));
     }
 
     @Override
     public void stop(GameData gameData, World world) {
-                System.out.println("I STARTED YAY");
 
     }
 
     @Override
     public void process(GameData gameData, World world) {
-                System.out.println("I STARTED YAY");
 
-        gameData.addEvent(new Event(EventType.PLAYER_SHOOT, world.getEntities(EntityType.PLAYER).get(0).getID()));
     }
 }
